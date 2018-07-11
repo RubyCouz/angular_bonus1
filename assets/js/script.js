d73a707ecd3886cc774ad9df950f251e34db6001
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-11 16:10:59 +0200

    finalisation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,1 @@
-//variable message erreur
-//var message = angular.module('email', ['ngMessages']);
-//variable regex input
-/*var regex = angular.module('regex', [])
-.controller('emailCtrl', ['$scope', function($scope) {
-  $scope.regex ='\\[a-z@.];'
-}]);*/
+angular.module('ngMessagesExample', ['ngMessages']);
