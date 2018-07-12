9ba53907a4139dd762160fefcba4ee47817bf7eb
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-12 10:32:00 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-angular.module('ngMessagesExample', ['ngMessages']);
+var form = angular.module('validForm', []);
+form.controller('validCtrl', ['$scope', function($scope){
+  $scope.regex = '^(.)?@(.)?$'
+  $scope.resultTestMail = false
+  $scope.testMail = function() {
+    $scope.resultTestMail = $scope.form.email.$valid
+  }
+}]);
